2a6870f2e5b7b128fefc71676fcab6bf3162296a
JJ Asghar <jj@chef.io>
2017-03-24 14:20:54 -0500

    Converted from jjasghar to rocketchat
    - Changing documentation from jjasghar to rocketchat
    
    Signed-off-by: JJ Asghar <jj@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-maintainer        'JJ Asghar'
-maintainer_email  'jj@chef.io'
+maintainer        'rocketchat'
+maintainer_email  'cookbook@rocket.chat'
 license           'Apache 2'
 description       'Installs/Configures Rocket.Chat'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.2.0'
 name              'rocketchat'
-issues_url        'https://github.com/jjasghar/chef-rocketchat/issues'
-source_url        'https://github.com/jjasghar/chef-rocketchat/'
+issues_url        'https://github.com/RocketChat/chef-rocketchat/issues'
+source_url        'https://github.com/RocketChat/chef-rocketchat/'
 
 recipe 'rocketchat', 'Installs and configures Rocket.Chat'
 
